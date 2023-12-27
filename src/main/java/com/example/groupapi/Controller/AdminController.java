d70f16eea5bbc4d2818315f70dd17009e88269a3
admin <ronakgupta083@gmail.com>
2023-12-27 01:04:53 -0800

    Add admin and user roles

--- src/main/java/com/example/groupapi/Controller/AdminController.java
+++ src/main/java/com/example/groupapi/Controller/AdminController.java
@@ -3,11 +3,17 @@ package com.example.groupapi.Controller;
 import com.example.groupapi.Dao.SignUpRequest;
 import com.example.groupapi.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin ")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hey Admin");
+    }
 }
--- src/main/java/com/example/groupapi/Controller/UserController.java
+++ src/main/java/com/example/groupapi/Controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.groupapi.Controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hey User");
+    }
+
 }
